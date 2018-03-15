afff57f8f2a436c70cfcc29e1b6f3898fae08ef5
Sabrinalulu <eg8625@gmail.com>
2018-03-15 13:02:18 +0800

    Modify .php ($GET -> $POST)
    GET:表示信封內不能裝信封的寄送方式
    POST:信封內有裝信件的寄送方式
    Reference: https://goo.gl/rvbniY

--- backend/receiveText.php
+++ backend/receiveText.php
@@ -4,7 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept");
 
-word_text = $_GET['word_text'];
+$word_text = $_POST['word_text'];
+print_r($word_text);
 $links = shell_exec('python executeModel.py $word_text');
 echo $links;
 
