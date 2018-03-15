e6754616606767c93f1ce460a78425189315cd4e
Sabrinalulu <eg8625@gmail.com>
2018-03-15 11:19:44 +0800

    Update receiveText.php

--- backend/receiveText.php
+++ backend/receiveText.php
@@ -1,6 +1,7 @@
 <?php
 //Cross-origin resource sharing header
-header("Access-Control-Allow-Origin: http://localhost:50000/");
+//http://localhost:50000/
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept");
 
